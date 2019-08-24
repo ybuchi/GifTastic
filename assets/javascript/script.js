0276548eba2926ce0f812e499fa956043eaf26c0
Yannik Buchi <yabuch_ch@hotmail.com>
2019-08-24 15:52:33 -0400

    The application now adds images to the divs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,7 +31,8 @@ function addCategory(userInput){
             var newButton = $("<button>");
             //Give the button a class and an id equal to the category value
             newButton.addClass("category-button");
-            newButton.attr("id", categoryArr[i]);
+            //Give the button a property of data-sport
+            newButton.attr("data-category", categoryArr[i]);
             //Add the text to the button
             newButton.text(categoryArr[i]);
             //Append the button to its parent container
@@ -47,7 +48,7 @@ function addCategory(userInput){
             //Give the button a class
             newButton.addClass("category-button");
             //Give the button an id equal to the category value
-            newButton.attr("id", categoryArr[i]);
+            newButton.attr("data-category", categoryArr[i]);
             //Add the text to the button
             newButton.text(categoryArr[i]);
             //Append the button to its parent container
@@ -81,19 +82,55 @@ $("#new-category").submit(function(event){
 
 //A J A X : 
 //When the user clicks a button that was created, use this button's ID to make an AJAX call
-$(document).on("click", ".category-button", function(){
+$(document).on("click",".category-button", function(){
+//Grab and store the data-category property of the button
+var category = $(this).attr("data-category");
+console.log("The value of the button I just clicked on is:" + category);
 
-var q = $(".category-button").attr("id");
-console.log("The value of the button I just clicked on is:" + q);
-
+//Construct the query URL
 var APIkey = "hZIRhlj4Dr05NlkiSLKE83kSVqKZ2cTZ";
 var gifLimit = 10;
-var queryURL = "https://api.giphy.com/v1/gifs/search?" + "api_key=" + APIkey + "&limit="+ gifLimit + "&offset=0&rating=G&lang=en" + "&q=" + q;
+var queryURL = "https://api.giphy.com/v1/gifs/search?" + "api_key=" + APIkey + "&limit="+ gifLimit + "&offset=0&rating=G&lang=en" + "&q=" + category;
 
 //Make the AJAX call that will grap 10 STATIC, non animated gifs and will display them on the page. 
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then( function(response){
+
+    var gifData = response.data;
+    console.log(gifData);
+    //Create and save references to the gifs and datapoints that we want to display
+    //We want the gif and the rating
+
+    //Display the data
+    for (i = 0; i < gifData.length; i++){
+    
+        var gifDiv = $("<div>");
+        var p = $("<p>").text("Rating: " + gifData[i].rating);
+        var gifImage = $("<img>");
+        gifImage.attr("src", gifData[i].images.original.url);
+
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+
+        $("#gif-display").append(gifDiv);
+}
+    
+    
+
+    //We then need to reset this function so that the next time you click on any button, it won't return the gifs for the first button you clicked on. 
+
 
 
 });
+
+
+
+
+});
+//^End of document click event
+
 
 
 });
